e13057a78384c9e815ed5a99dd0b844ef200fcab
Gill <gill.lumer.klabbers@gmail.com>
2019-10-10 15:13:50 +0200

    Uge4 PCA with updates

--- L04/Uge4_PCA_1.py
+++ L04/Uge4_PCA_1.py
@@ -1,10 +1,6 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Thu Sep 19 10:56:47 2019
-
-@author: Gill
-"""
+# pylint: disable=no-member
 #%% Imports and data preparation
+
 import numpy as np
 from sklearn.datasets import load_digits
 from sklearn.decomposition import PCA
@@ -20,9 +16,11 @@ y = digits.target
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
 
 #%% Plot single digit
-def plot_digit(digit, cmpsize=64):
+def plot_digit(digit, cmpsize=64, title=None):
     s = int(np.sqrt(cmpsize))
     image = digit.reshape(s, s)
+    plt.figure(figsize=(2,2))
+    plt.title(title)
     plt.imshow(image, cmap = matplotlib.cm.binary, interpolation="nearest")
     plt.axis("off")
     plt.show()
@@ -37,25 +35,36 @@ def create_pca(data, cmps):
     pca.fit(data)
     return pca
 
-dimensions = [2, 4, 8, 9, 16, 25, 32]
+dimensions_sq = [n**2 for n in range(1,7)] # 4, 9, 16 ...
+dimensions_2n = [2**n for n in range(1,7)] # 2, 4, 8 ...
+dimensions = sorted(set(dimensions_sq + dimensions_2n))
+
 pca = {n: create_pca(X_train, n) for n in dimensions}
 
 X_reduced = {n: pca[n].fit_transform(X_train) for n in dimensions}
 
+print(f"Created {len(dimensions)} pca's of training data with dimensions {dimensions}")
 
 #%% Plot the standard deviation and explained variance pr dimension
 def plot_pca_dim_variance(pca):
     varians = pca.explained_variance_
     s = np.sqrt(varians)
+
+    plt.figure(figsize=(8,4))
+    plt.suptitle(f"Components: {pca.n_components}", x=0.5, y=1.05)
     
+    plt.subplot(1, 2, 1, adjustable='box')
     plt.plot(s)
-    plt.title('Standard afvigelser (spredning) langs de forskellige dimensioner')
+    plt.title('Standard afvigelser (spredning) \nlangs de forskellige dimensioner')
     plt.xlabel('Dimension nummer')
     
-    plt.figure()
+    plt.subplot(1, 2, 2, adjustable='box')
     plt.plot(np.cumsum(pca.explained_variance_)/np.sum(pca.explained_variance_))
     plt.title('Explained variance - procentdel')
     plt.xlabel('Dimension nummer')
+    
+for n in [32, 25, 16, 8, 4]:
+    plot_pca_dim_variance(pca[n])
 
 #%% Split data into which digit it "should" represent
 def mkdict1(x_reduced): # Method 1
@@ -69,38 +78,59 @@ def mkdict2(x_reduced): # Method 2
         digit: np.array([x for xi, x in enumerate(x_reduced) if y_train[xi] == digit]) 
         for digit in range(10)
     }
- 
-#%% Time the two different mothods of creating dict
 
+#%% Time the two different mothods of creating dict
 from timeit import timeit
 r1 = timeit(lambda: mkdict1(X_reduced[2]), number=100)
 r2 = timeit(lambda: mkdict2(X_reduced[2]), number=100)
 print(f"mkdict1 = {r1}")
 print(f"mkdict2 = {r2}")
+print("the fastest is {}".format("mkdict1" if r1 < r2 else "mkdict2"))
 
 #%% Plot the two-component representation of digits and color by 'actual' digit
 npdict = mkdict1(X_reduced[2])
 def plot_two_component_digits(digits):
     for dnum in digits:
         plt.scatter(npdict[dnum][:,0], npdict[dnum][:,1], s=2, label=str(dnum))
-    plt.axis('equal')
+    # plt.axis('equal')
     plt.legend()
     plt.show()
 
 plot_two_component_digits([1, 2, 3])
 plot_two_component_digits([4, 5, 6])
 plot_two_component_digits([7, 8, 9])
-plot_two_component_digits([1, 3])
-plot_two_component_digits([8, 9])
 
+#%% plot every pairwise digit combination
+pairperms = [(a, b) for a in range(10) for b in range(a+1, 10)] 
+
+for pair in pairperms:
+    plot_two_component_digits(pair)
+#%% 
+# plot 3-pair combinations with (probably) high/good seperation
+pl ={
+    0:[], 1:[],
+    2:[6],
+    3:[4,6],
+    4:[5,7,8,9],
+    5:[6],
+    6:[7,8],
+    7:[], 8:[], 9:[]
+}
+
+for n in range(10):
+    for m in pl[n]:
+        plot_two_component_digits([0, n, m])
 #%% Recreate / recover original
 X_recovered = {n: pca[n].inverse_transform(X_reduced[n]) for n in dimensions}
 
-#%% Plot 16-component PCA representation of digits as 4x4 grid
-for i in range(10):
-    plot_digit(X_train[i])
-    print(y_train[i])
-    plot_digit(X_reduced[16][i], 16)
+#%% Plot 16-component PCA representation of digits as grid
+indexes = [1,2,3,41,42,43]
+
+for i in indexes:
+    plot_digit(X_train[i], title=y_train[i])
+    # print(y_train[i])
+    for d in dimensions_sq:
+        plot_digit(X_reduced[d][i], d, title=y_train[i])
 
 #%%
 digit_index = 5
